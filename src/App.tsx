30719b3fc41f80b90abbc480dde231edae24cc66
witoldmetel <witoldmetel@gmail.com>
2021-12-03 15:22:11 +0100

    add prettier and linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
-import './App.css';
-
-function App() {
-	return <div className="App">Main</div>;
+export function App() {
+  return <div className="App">Main</div>;
 }
-
-export default App;
