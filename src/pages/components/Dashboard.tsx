c48ffa5f58f153a46eff7cff966d2dacf3437adf
witoldmetel <witoldmetel@gmail.com>
2021-12-04 13:43:35 +0100

    fix

--- src/pages/components/Dashboard.tsx
+++ src/pages/components/Dashboard.tsx
@@ -1,22 +1,28 @@
-import { useState } from 'react';
-
 import { Container, Typography, Grid, Button } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 
 import { InitCard, InitCardProps } from 'src/core/components';
 import { initCards, INIT_CARD_TITLE } from 'src/constants';
 
-export function Dashboard() {
-  const [selectedCard, setSelectedCard] = useState(INIT_CARD_TITLE.RANDOM);
+type DashboardProps = {
+  selectedCard: INIT_CARD_TITLE;
+
+  setSelectedCard: (selectedCard: INIT_CARD_TITLE) => void;
+  onPlayClick: (isGameActive: boolean) => void;
+};
+
+export function Dashboard({ selectedCard, setSelectedCard, onPlayClick }: DashboardProps) {
+  const classes = useStyles();
 
   return (
-    <Container maxWidth="lg" component="main" sx={{ p: 5 }}>
+    <Container className={classes.root} maxWidth="lg" component="main">
       <Typography component="h1" variant="h2" align="center" color="text.primary" gutterBottom>
         Star Wars Card Game
       </Typography>
       <Typography variant="h5" align="center" color="text.secondary" component="p">
         Choose one of the resources and participate in the galactic battle!
       </Typography>
-      <Grid container spacing={5} alignItems="flex-start" sx={{ p: 5 }}>
+      <Grid className={classes.cardsWrapper} container spacing={5} alignItems="flex-start">
         {initCards.map((card: Pick<InitCardProps, 'title' | 'icon' | 'description'>) => (
           <InitCard
             key={card.title}
@@ -26,7 +32,25 @@ export function Dashboard() {
           />
         ))}
       </Grid>
-      <Button variant="contained">Play</Button>
+      <Button className={classes.button} variant="contained" onClick={() => onPlayClick(true)}>
+        Play
+      </Button>
     </Container>
   );
 }
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 40
+  },
+  cardsWrapper: {
+    padding: 30
+  },
+  button: {
+    height: 50,
+    width: 100
+  }
+}));
--- src/pages/components/Gameboard.tsx
+++ src/pages/components/Gameboard.tsx
@@ -1,11 +1,22 @@
-import { Container, Typography } from '@mui/material';
+import { Container, Typography, Button } from '@mui/material';
 
-export function Gameboard() {
+import { INIT_CARD_TITLE } from 'src/constants';
+
+type GameboardProps = {
+  selectedCard: INIT_CARD_TITLE;
+
+  onPlayClick: (isGameActive: boolean) => void;
+};
+
+export function Gameboard({ selectedCard, onPlayClick }: GameboardProps) {
   return (
     <Container maxWidth="lg" component="main" sx={{ p: 5 }}>
       <Typography component="h1" variant="h2" align="center" color="text.primary" gutterBottom>
-        Game Board
+        {`${selectedCard} Battle`}
       </Typography>
+      <Button variant="contained" onClick={() => onPlayClick(false)}>
+        Start New Game
+      </Button>
     </Container>
   );
 }
