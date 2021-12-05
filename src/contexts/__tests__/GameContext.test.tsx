23ec3f7be11be1502c211404a33fb2096ff1ba09
witoldmetel <witoldmetel@gmail.com>
2021-12-05 16:34:27 +0100

    part 3

--- src/contexts/__tests__/GameContext.test.tsx
+++ src/contexts/__tests__/GameContext.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/button-has-type */
 import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
 import { QueryClientProvider } from 'react-query';
@@ -58,6 +59,206 @@ describe('GameProvider', () => {
       fireEvent.click(getByText('Select Random'));
 
       expect(getByText(`Resource: ${INIT_CARD_TITLE.RANDOM}`)).toBeTruthy();
+    });
+  });
+
+  describe('getBattleResult', () => {
+    it('character battle', () => {
+      const firstPlayer = {
+        mass: '2',
+        name: 'Joe',
+        birth_year: 'unknown',
+        gender: 'unknown',
+        height: 'unknown'
+      };
+
+      const secondPlayer = {
+        mass: '3',
+        name: 'Doe',
+        birth_year: 'unknown',
+        gender: 'unknown',
+        height: 'unknown'
+      };
+
+      const { getByText } = render(
+        <QueryClientProvider client={queryClient}>
+          <GameProvider>
+            <GameContext.Consumer>
+              {(value) => {
+                const battleResult = value?.getBattleResult(firstPlayer, secondPlayer);
+
+                return (
+                  <>
+                    <span>{battleResult}</span>
+                    <button onClick={() => value?.onResourceSelect(INIT_CARD_TITLE.CHARACTER)}>Select Character</button>
+                  </>
+                );
+              }}
+            </GameContext.Consumer>
+          </GameProvider>
+        </QueryClientProvider>
+      );
+
+      fireEvent.click(getByText('Select Character'));
+
+      expect(getByText('The winner is Doe')).toBeTruthy();
+    });
+
+    it('starship battle', () => {
+      const firstPlayer = {
+        crew: '22',
+        name: 'Solar Joe',
+        model: 'unknown',
+        manufacturer: 'unknown',
+        length: 'unknown'
+      };
+
+      const secondPlayer = {
+        crew: '13',
+        name: 'Solar Doe',
+        model: 'unknown',
+        manufacturer: 'unknown',
+        length: 'unknown'
+      };
+
+      const { getByText } = render(
+        <QueryClientProvider client={queryClient}>
+          <GameProvider>
+            <GameContext.Consumer>
+              {(value) => {
+                const battleResult = value?.getBattleResult(firstPlayer, secondPlayer);
+
+                return (
+                  <>
+                    <span>{battleResult}</span>
+                    <button onClick={() => value?.onResourceSelect(INIT_CARD_TITLE.STARSHIP)}>Select Starship</button>
+                  </>
+                );
+              }}
+            </GameContext.Consumer>
+          </GameProvider>
+        </QueryClientProvider>
+      );
+
+      fireEvent.click(getByText('Select Starship'));
+
+      expect(getByText('The winner is Solar Joe')).toBeTruthy();
+    });
+
+    it('draw', () => {
+      const firstPlayer = {
+        crew: '2',
+        name: 'Solar Joe',
+        model: 'unknown',
+        manufacturer: 'unknown',
+        length: 'unknown'
+      };
+
+      const secondPlayer = {
+        crew: '2',
+        name: 'Solar Doe',
+        model: 'unknown',
+        manufacturer: 'unknown',
+        length: 'unknown'
+      };
+
+      const { getByText } = render(
+        <QueryClientProvider client={queryClient}>
+          <GameProvider>
+            <GameContext.Consumer>
+              {(value) => {
+                const battleResult = value?.getBattleResult(firstPlayer, secondPlayer);
+
+                return (
+                  <>
+                    <span>{battleResult}</span>
+                    <button onClick={() => value?.onResourceSelect(INIT_CARD_TITLE.STARSHIP)}>Select Starship</button>
+                  </>
+                );
+              }}
+            </GameContext.Consumer>
+          </GameProvider>
+        </QueryClientProvider>
+      );
+
+      fireEvent.click(getByText('Select Starship'));
+
+      expect(getByText('We have draw!')).toBeTruthy();
+    });
+
+    it('unknown result', () => {
+      const firstPlayer = {
+        crew: 'unknown',
+        name: 'Solar Joe',
+        model: 'unknown',
+        manufacturer: 'unknown',
+        length: 'unknown'
+      };
+
+      const secondPlayer = {
+        crew: '2',
+        name: 'Solar Doe',
+        model: 'unknown',
+        manufacturer: 'unknown',
+        length: 'unknown'
+      };
+
+      const { getByText } = render(
+        <QueryClientProvider client={queryClient}>
+          <GameProvider>
+            <GameContext.Consumer>
+              {(value) => {
+                const battleResult = value?.getBattleResult(firstPlayer, secondPlayer);
+
+                return (
+                  <>
+                    <span>{battleResult}</span>
+                    <button onClick={() => value?.onResourceSelect(INIT_CARD_TITLE.STARSHIP)}>Select Starship</button>
+                  </>
+                );
+              }}
+            </GameContext.Consumer>
+          </GameProvider>
+        </QueryClientProvider>
+      );
+
+      fireEvent.click(getByText('Select Starship'));
+
+      expect(getByText('Unknown result of the battle')).toBeTruthy();
+    });
+
+    it('wrong data', () => {
+      const firstPlayer = {
+        crew: 'unknown',
+        name: 'Solar Joe',
+        model: 'unknown',
+        manufacturer: 'unknown',
+        length: 'unknown'
+      };
+
+      const secondPlayer = {
+        crew: '2',
+        name: 'Solar Doe',
+        model: 'unknown',
+        manufacturer: 'unknown',
+        length: 'unknown'
+      };
+
+      const { getByText } = render(
+        <QueryClientProvider client={queryClient}>
+          <GameProvider>
+            <GameContext.Consumer>
+              {(value) => {
+                const battleResult = value?.getBattleResult(firstPlayer, secondPlayer);
+
+                return <span>{battleResult}</span>;
+              }}
+            </GameContext.Consumer>
+          </GameProvider>
+        </QueryClientProvider>
+      );
+
+      expect(getByText('Something went wrong')).toBeTruthy();
     });
   });
 });
